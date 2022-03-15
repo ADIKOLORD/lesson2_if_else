733792b3c0b98ceccf2ce231a4286224e0b38fab
ADIKOLORD <adiko23082004@gmail.com>
2022-03-15 10:15:30 +0600

    lesson about if else

---
+++ lesson2.py
@@ -0,0 +1,152 @@
+'''
+print('Problem 1')
+a = 2**3
+b = 3**2
+if a > b:
+    print('a > b')
+else:
+    print('a < b')
+
+print('Problem 2')
+number = int(input('Enter number: '))
+if number >= 0 and number <= 21 or number >= 57 and number <= 100:
+    print('Welcome')
+else:
+    print('error')
+
+print('Problem 3')
+
+number = int(input('Enter number: '))
+if number % 2 == 0:
+    print('Четное')
+else:
+    print('Не четное')
+if number % 3 == 0:
+    print('На три делится без остатка')
+else:
+    print('Не делится на три  без остатка')
+if number ** 2 > 1000:
+    print('kvadrat bolshe 1000')
+else:
+    print('kvadrat ne bolshe 1000')
+
+print('Problem 4')
+if True:
+    print('Everywhere True')
+
+print('Problem 5')
+a = 10 // 5
+b = 10 / 5
+if a == b:
+    print(a + b)
+else:
+    print('Ne ravny')
+
+print('Problem 6')
+
+number = int(input('Enter number: '))
+if number < 0:
+    print(number)
+else:
+    print('Your number bolshe 0')
+
+
+print('Problem 7')
+a = 10
+b = 5
+if a > 0 and b > 0:
+    print('They are + digital')
+
+print('Problem 8')
+if a > b:
+    print(a + 2)
+else:
+    print(b + 2)
+
+print('Problem 9')
+user_num = int(input("Enter number: "))
+if user_num > 0:
+    print('Your number is polojitelnyi')
+elif user_num < 0:
+    print('Your number is Отрицательный')
+else:
+    print(number)
+
+print('Problem 10')
+user_age = int(input('Enter your age: '))
+if user_age >= 18:
+    print('Совершеннолетний')
+elif user_age <= 4:
+    print('Ребенок')
+else:
+    print('Не Совершеннолетний')
+
+print('Problem 11')
+a, b = 45, 6
+if a % b == 0:
+    print('a делится на b без остатка')
+else:
+    print('a не делится на b без остатка')
+
+print('Problem 12')
+year = int(input(''))
+if year == 2022:
+    print('Todays year')
+elif year > 2022:
+    print('Ne nastupil')
+else:
+    print('Proshel year')
+
+print('Problem 14')
+one = int(input('First number: '))
+two = int(input('Second number: '))
+three = int(input('Thrid number: '))
+if one >= two and one >= three:
+    print(one)
+elif two >= one and two >= three:
+    print(two)
+else:
+    print(three)
+
+print('Problem 15')
+a, b, c = 17391 % 17, 546 % 17, 934 % 17
+if a >= b and a >= c:
+    print(a)
+elif b >= a and b >= c:
+    print(b)
+else:
+    print(c)
+
+print('Problem 16')
+x = 13
+if x**2 < 172:
+    print((x**2)**2)
+else:
+    print(x)
+'''
+
+########################################
+# f, r, j = 10, 10.5, 13.7
+# if r > j:
+#     print('r > j')
+# elif r < j:
+#     print('r < j')
+# else:
+#     print('r = j')
+#
+# num = 4388
+# if num % 7 >= 4:
+#     print((num % 7) * 3)
+# else:
+#     print()
+
+
+
+
+
+
+
+
+
+
+
